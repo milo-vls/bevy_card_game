8a42bb66eb737ada1cffa752361d3e546c377f0a
Hepriz <vilas.milo@gmail.com>
2025-01-14 02:46:07 +0100

    remove useless file

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,6 @@ fn setup(
     // camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0., 35., 0.).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
